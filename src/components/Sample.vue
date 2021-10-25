fa1f791d162ecb0341c334d381f59be7b93ce496
Ali Bidkhori <a.bidkhori2004@gmail.com>
2021-10-25 17:15:02 +0200

    Update 1.2 -  Lets begin with sandbox

---
+++ src/components/Sample.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="logo">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>
+      {{ "Fullname : " + fullname }}
+    </h1>
+    <p>
+      {{ "Age : " + age }}
+    </p>
+    <p v-if="age > 16">
+      {{ firstname + " is older than 16 " }}
+    </p>
+    <p v-else>
+      {{ firstname + " is younger than 16 " }}
+    </p>
+    <strong> My favorite fruits : </strong>
+    <ul>
+      <li v-for="(fruit, index) in fruits" :key="index">
+        {{ fruit }}
+      </li>
+    </ul>
+    <button @click="changeAge(age)">click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sample',
+  props: {
+    firstname: {
+      type: String,
+      default: ''
+    },
+    lastname: {
+      type: String,
+      default: ''
+    },
+    age: {
+      type: Number,
+      default: 0
+    },
+    fruits: {
+      type: Array,
+      default: () => []
+    }
+  },
+  computed: {
+    fullname () {
+      const output = this.firstname + ' ' + this.lastname
+      return output
+    }
+  },
+  methods: {
+    changeAge (age) {
+      this.$emit('age', age)
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <Sample
+      :firstname="firstname"
+      :lastname="lastname"
+      :age="age"
+      :fruits="fruits"
+      @age="changeAge"
+    />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Sample from '@/components/Sample.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Sample
+  },
+
+  data () {
+    return {
+      firstname: 'Ali',
+      lastname: 'Bidkhori',
+      age: '15',
+      fruits: ['apple', 'orange', 'khiar', 'mooz']
+    }
+  },
+
+  methods: {
+    changeAge (age) {
+      this.age = age + 1
+    }
+  }
+}
+</script>
